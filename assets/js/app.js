29fecf3f0805e98e48167a7f146dc7d4b7c04793
SuryaTej-N <sxn2261@mavs.uta.edu>
2024-03-03 04:47:00 -0600

    changes in navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ document.body.appendChild(script);
 
 let header = $(`
 <nav class="navbar navbar-expand-lg fixed-top dark-theme" id="navbar">
-<a class="navbar-brand" href="index.html">Surya Teja </a>
+<a class="navbar-brand" href="https://zoomquilt.org/">Surya Teja </a>
 <div class="hamburger_wrapper navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 
   <div id="js-hamburger" class="hamburger">
@@ -28,7 +28,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://surya-codesense.vercel.app/" target="_blank">AI_model</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://surya-codesense.vercel.app/" target="_blank">Surya's AI</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
    </li>
