37bf8cc7c07938c063c2b76d5794c4d17dba0888
SuryaTej-N <sxn2261@mavs.uta.edu>
2024-03-03 02:25:24 -0600

    changes in research

--- assets/js/research.js
+++ assets/js/research.js
@@ -11,7 +11,7 @@ const research = [
       "2022 International Conference on Public Key Infrastructure",
     researchYr: 2022,
     citebox: "popup1",
-    image: "assets/images/research-page/inteferenceNetwork.png",
+    image: "assets/images/research-page/PKI.png",
     citation: {
       vancouver:
         "Surya Teja Neerukattu, Enhanced Public Key Infrastructure for Public e-Services in India””,Journal of Science and Technology, Vol. 07, Issue 06,-August 2022, pp20-30.",
@@ -28,10 +28,10 @@ const research = [
       "Sathyabama university, chennai, 2020",
     researchYr: 2020,
     citebox: "popup2",
-    image: "assets/images/research-page/crossLingual.png",
+    image: "assets/images/research-page/IoT-Based-Solar-Power-Monitoring_res.jpg",
     citation: {
       vancouver:
-        "Mikel Artetxe, Sebastian Ruder, Dani Yogatama, Gorka Labaka and Eneko Agirre. A Call for More Rigor in Unsupervised Cross-lingual Learning. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
+        "Surya Teja Neerukattu, REMOTELY MONITORING HEALTH OF THE SOLAR POWER SYSTEM USING MQTT, Sathyabama University, 2020.",
     },
     abstract:
       "A wireless remote monitoring system for solar power system is proposed in this paper. It is an Internet of Things (IoT) application implemented with an objective to offer a cost-effective solution of monitoring system, which continuously presents parameters of solar system and its performance on mobile app or website using the cloud server like adafruit . so, This system is developed with solar panel to monitor string voltage, string current, voltage from battery. System is controlled by ATmega328p microcontroller which is in Arduino Uno. On board Node mcu 8266, GSM enhances the system performance with reduced area and facilitates to monitor system parameters after every minute.",
@@ -45,7 +45,7 @@ const research = [
       "The 58th Annual Meeting of the Association for Computational Linguistics",
     researchYr: 2020,
     citebox: "popup2",
-    image: "assets/images/research-page/crossLingual.png",
+    image: "assets/images/research-page/unsupervised learning_res.png",
     citation: {
       vancouver:
         "Mikel Artetxe, Sebastian Ruder, Dani Yogatama, Gorka Labaka and Eneko Agirre. A Call for More Rigor in Unsupervised Cross-lingual Learning. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
