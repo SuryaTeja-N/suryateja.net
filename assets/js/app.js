077d7f98f3f77eed14599669835e1d243774d9b1
SuryaTej-N <sxn2261@mavs.uta.edu>
2024-03-02 22:35:59 -0600

    changes completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,7 +28,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://ai.suryateja.net/" target="_blank">AI_model</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://surya-codesense.vercel.app/" target="_blank">AI_model</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
    </li>
